2c2ab426e8b4a27e0f4a54cc038172fb12d5bdee
watepu <40508321+watepu@users.noreply.github.com>
2018-08-26 02:13:50 +0000

    課題を作成

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,48 @@
+class BlogsController < ApplicationController
+  
+  def index
+    @blogs = Blog.all
+  end
+  
+  def new
+    @blog = Blog.new
+  end
+  
+  def create
+    @blog = Blog.create(blog_params)
+    if @blog.save
+      redirect_to blogs_path
+    else
+      render 'new'
+    end
+  end
+  
+  def edit
+    @blog = Blog.find(params[:id])
+  end
+  
+  def update
+    @blog = Blog.find(params[:id])
+    if @blog.update(blog_params)
+      redirect_to blogs_path
+    else
+      render 'edit'
+    end
+  end
+  
+  def destroy
+    @blog = Blog.find(params[:id])
+    @blog.destroy
+    redirect_to blogs_path
+  end
+  
+  def confirm
+    @blog = Blog.new(blog_params)
+  end
+  
+  private
+  def blog_params
+    params.require(:blog).permit(:title, :content)
+  end
+  
+end
