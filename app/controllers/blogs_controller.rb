c85cabf0b4a3d11d159778aa2dfb05c628e3e486
watepu <40508321+watepu@users.noreply.github.com>
2018-09-04 19:46:31 +0900

    ログイン機能追加

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,13 +1,14 @@
 class BlogsController < ApplicationController
-  
+  before_action :forcibly_redirect, only:[:new,:edit,:show,:destroy]
+
   def index
     @blogs = Blog.all
   end
-  
+
   def new
     @blog = Blog.new
   end
-  
+
   def create
     @blog = Blog.create(blog_params)
     if @blog.save
@@ -16,11 +17,11 @@ class BlogsController < ApplicationController
       render 'new'
     end
   end
-  
+
   def edit
     @blog = Blog.find(params[:id])
   end
-  
+
   def update
     @blog = Blog.find(params[:id])
     if @blog.update(blog_params)
@@ -29,21 +30,27 @@ class BlogsController < ApplicationController
       render 'edit'
     end
   end
-  
+
   def destroy
     @blog = Blog.find(params[:id])
     @blog.destroy
     redirect_to blogs_path
   end
-  
+
   def confirm
     @blog = Blog.new(blog_params)
     render :new if @blog.invalid?
   end
-  
+
   private
   def blog_params
     params.require(:blog).permit(:title, :content)
   end
-  
+
+  def forcibly_redirect
+    if current_user.present? == false
+      redirect_to new_session_path
+    end
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :sessions
+
   get '/' => "home#top"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
@@ -7,5 +9,7 @@ Rails.application.routes.draw do
       post :confirm
     end
   end
+
+  resources :users
 
 end
