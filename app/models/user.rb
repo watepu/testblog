efc78dd21ee3afdceb10eb0412e4afd2c9f0db17
watepu <40508321+watepu@users.noreply.github.com>
2018-11-17 10:28:47 +0900

    user.rbにフォロー、フォロワー追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,5 +16,6 @@ class User < ApplicationRecord
   has_many :active_relationships, foreign_key: "follower_id", class_name: "Relationship", dependent: :destroy
   has_many :passive_relationships, foreign_key: "followed_id", class_name: "Relationship", dependent: :destroy
   has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships, source: :follower
 
 end
