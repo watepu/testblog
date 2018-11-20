655ad0cc933213ea222b6480e86ec82a7bc59e5d
watepu <40508321+watepu@users.noreply.github.com>
2018-11-20 20:00:37 +0900

    create,privateメソッド記述

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -23,4 +23,19 @@ class MessagesController < ApplicationController
 
     @message = @conversation.messages.build
   end
+
+  def create
+    @message = @conversation.messages.build(message_params)
+    if @message.save
+      redirect_to conversation_messages_path(@conversation)
+    else
+      render 'index'
+    end
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:body, :user_id)
+  end
 end
